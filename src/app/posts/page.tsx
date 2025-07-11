a889a9dece932bb2c592706d2052e443ecde94d8
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-07-11 19:01:48 +0800

    feat: create common component(stack, group, post) & hooks (next auth)

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,16 @@
+// import { useEffect, useState } from "react";
+
+// interface AuthorProps{
+//   authorId: Number,
+//   authorName: String
+// }
+// export default function PostHome() {
+//   const [author, setAuthor] = useState<AuthorProps[]>([]);
+//   const fetchData = async () => {
+//     try{
+//       const response = await fetch('/api/category')
+//       setAuthor(response)
+//     }
+//   }
+//   return <div>Test</div>;
+// }
